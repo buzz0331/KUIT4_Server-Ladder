302f27218d07559b464f7b3714cd9761bb8dd1ad
janghyunjun <buzz03312@naver.com>
2024-09-19 15:49:23 +0900

    Test코드 작성

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,92 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+
+    @Test
+    public void 사다리_생성() throws Exception {
+        //given
+        int numberOfPerson = 3;
+        int row = 3;
+        Ladder ladder = new Ladder(row, numberOfPerson);
+
+        //when
+
+        //then
+        assertThat(ladder.getRows().length).isEqualTo(3);
+    }
+
+    @Test
+    public void 사다리_라인생성() throws Exception {
+        //given
+        int numberOfPerson = 2;
+        int row = 2;
+        Ladder ladder = new Ladder(row, numberOfPerson);
+
+        //when
+        Position position = new Position(NaturalNumber.from(1), NaturalNumber.from(1));
+        ladder.drawLine(position);
+
+        //then
+        assertThat(ladder.getRows()[0][0]).isEqualTo(1);
+        assertThat(ladder.getRows()[0][1]).isEqualTo(-1);
+    }
+
+    @Test
+    public void 사다리_라인생성_범위처리() throws Exception {
+        //given
+        int numberOfPerson = 4;
+        int row = 2;
+        Ladder ladder = new Ladder(row, numberOfPerson);
+
+        //when
+        Position position = new Position(NaturalNumber.from(2), NaturalNumber.from(4));
+
+        //then
+        assertThatThrownBy(() -> ladder.drawLine(position))
+                .isInstanceOf(IllegalArgumentException.class);
+
+
+    }
+    @Test
+    public void 사다리_라인생성_예외처리() throws Exception {
+        //given
+        int numberOfPerson = 2;
+        int row = 2;
+        Ladder ladder = new Ladder(row, numberOfPerson);
+
+        //when
+        Position position = new Position(NaturalNumber.from(1), NaturalNumber.from(1));
+        Position position1 = new Position(NaturalNumber.from(1), NaturalNumber.from(2));
+        ladder.drawLine(position);
+//        ladder.drawLine(position);
+
+        //then
+        assertThatThrownBy(() -> ladder.drawLine(position1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 사다리_이동() throws Exception {
+        //given
+        int numberOfPerson = 3;
+        int row = 4;
+        Ladder ladder = new Ladder(row, numberOfPerson);
+
+
+        //when
+        ladder.drawLine(new Position(NaturalNumber.from(1), NaturalNumber.from(1)));
+        ladder.drawLine(new Position(NaturalNumber.from(2), NaturalNumber.from(2)));
+        ladder.drawLine(new Position(NaturalNumber.from(3), NaturalNumber.from(2)));
+        ladder.drawLine(new Position(NaturalNumber.from(4), NaturalNumber.from(2)));
+        int result = ladder.run(NaturalNumber.from(1));
+
+        //then
+        assertThat(result).isEqualTo(3);
+
+    }
 
 }
