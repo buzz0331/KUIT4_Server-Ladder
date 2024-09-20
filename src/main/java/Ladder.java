87369f083b17f6901c73c07ccef20bba0b1f4d5b
janghyunjun <buzz03312@naver.com>
2024-09-20 15:51:33 +0900

    refactor: firstLine, middleLine 메서드 합침

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -20,7 +20,7 @@ public class Ladder {
     }
 
     public void drawLine(Position position) {
-        position.verify(rows);
+        verify(position);
         this.rows[position.getX()][position.getY()] = 1; //오른쪽으로 가는 선
         this.rows[position.getX()][position.getY()+1] = -1; //왼쪽으로 가는 선
     }
@@ -37,4 +37,46 @@ public class Ladder {
 
         return personNum+1;
     }
+
+    private void verify(Position position) {
+        final int x = position.getX();
+        final int y = position.getY();
+        //제대된 라인인지 검증하는 함수
+        if (x > rows.length - 1) {
+            //높이 초과
+            throw new IllegalArgumentException("잘못된 라인 생성입니다");
+        }
+        if (y == 0) {
+            //첫번째 줄일때
+            checkingLine(x, y, Sequence.FIRST);
+        }
+        if (y >= rows[0].length-1) {
+            //마지막 줄은 라인 생성 불가
+            throw new IllegalArgumentException("잘못된 라인 생성입니다.");
+        }
+        if (y > 0 && y < rows[0].length-1) {
+            //중간 줄일때
+            checkingLine(x, y, Sequence.MIDDLE);
+        }
+    }
+
+    private void checkingLine(int x, int y, Sequence sequence) {
+        if (rows[x][y] != 0) {
+            //자신
+            throw new IllegalArgumentException("잘못된 라인 생성입니다.");
+        }
+        if (y+1 != rows[0].length && rows[x][y + 1] != 0) {
+            //선이 그려지는 오른쪽
+            throw new IllegalArgumentException("잘못된 라인 생성입니다.");
+        }
+        if (y+2 != rows[0].length && rows[x][y + 2] != 0) {
+            //두칸 오른쪽
+            throw new IllegalArgumentException("잘못된 라인 생성입니다.");
+        }
+        if (sequence == Sequence.MIDDLE && y-1 != 0 && rows[x][y - 1] != 0) {
+            //왼쪽 (가운데 라인을 생성하는 경우만 체크)
+            throw new IllegalArgumentException("잘못된 라인 생성입니다.");
+        }
+    }
+
 }
---
+++ src/main/java/Sequence.java
@@ -0,0 +1,4 @@
+public enum Sequence {
+    //라인을 그리는 줄 위치
+    FIRST, MIDDLE
+}
